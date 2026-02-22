f36487c1c1120563b59b4db35b718d3db49685c4
mondalsounisa-afk <mondalsounisa@gmail.com>
2026-02-22 20:07:17 +0530

    Add Java assignment programs
    Initial commit of Java lab assignment files

---
+++ AddNumbers.java
@@ -0,0 +1,10 @@
+public class AddNumbers {
+    public static void main(String[] args) {
+        int c = 10;
+        int d = 20;
+        int sum = c + d;
+
+        System.out.println("Sum = " + sum);
+    }
+}
+
---
+++ ReverseNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        int reversed = 0;
+        int temp = number;
+        while (temp != 0) {
+            int digit = temp % 10;      
+            reversed = reversed * 10 + digit; 
+            temp /= 10;                  
+        }
+        System.out.println("Reversed number: " + reversed);
+        sc.close();
+    }
+}
